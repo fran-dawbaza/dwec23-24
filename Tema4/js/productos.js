ea40d33d307b8ec5ca9fb7148f1bfaeec922a304
fran-dawbaza <fgargar092@g.educaand.es>
2024-02-02 10:19:09 +0100

    paginacion

--- Tema4/js/productos.js
+++ Tema4/js/productos.js
@@ -3,17 +3,19 @@ fetch('./assets/productos.json')
 .then(datos=>{
     productos = datos.products;
     muestraProductos(productos); //aquí sí
+    //filtraProductosCon('');
 });
 
 function muestraProductos(p){
-    const filas = p.map(p=>`      <tr>
+    const filas = p.map((p,indice)=>`      <tr class="${indice>7?'ocultoPaginacion':''}">
     <th scope="row">${p.title}</th>
     <td>${p.price}</td>
     <td><img  width="75px" src="${p.thumbnail}"/></td>
     <td>
     <i data-id-producto="${p.id}" class="bi bi-eye" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#productoModal"></i></td>
   </tr>`).join('');
-    const tabla = `    <table id="mis-productos" class="table">
+    const paginacion = obtenPaginacion();
+    const tabla = `${paginacion}    <table id="mis-productos" class="table">
     <thead>
       <tr>
         <th scope="col">Nombre</th>
@@ -33,6 +35,22 @@ function muestraProductos(p){
     miDiv.addEventListener('click', (e)=>{
         muestraProducto(e,p);
     });
+}
+
+function obtenPaginacion(pagina=1){
+  return `<nav aria-label="Page navigation">
+  <ul class="pagination justify-content-end">
+    <li class="page-item disabled">
+      <a class="page-link">&lt;</a>
+    </li>
+    <li class="page-item"><a class="page-link" href="#">1</a></li>
+    <li class="page-item"><a class="page-link" href="#">2</a></li>
+    <li class="page-item"><a class="page-link" href="#">3</a></li>
+    <li class="page-item">
+      <a class="page-link" href="#">&gt;</a>
+    </li>
+  </ul>
+</nav>`;
 }
 
 function muestraProducto(evento,productos){
@@ -60,11 +78,39 @@ function filtraProductosCon(texto){
   //console.log('ahí va');
   for (let fila of tabla.tBodies[0].rows){
     if (fila.textContent.toLowerCase().includes(textoEnMinuscula))
-        fila.classList.remove('filtrado');
+        fila.classList.remove('filtradoTexto');
     else
-        fila.classList.add('filtrado');
+        fila.classList.add('filtradoTexto');
   }
+  filtraProductosPorPagina(1);
 }
+
+function filtraProductosPorPagina(pagina=1){
+  const tabla = document.getElementById('mis-productos');
+  if (!tabla) return;
+  const maxProductosPorPagina = 8;
+
+  const filasSinFiltradoTexto =  Array
+                                .from(tabla.tBodies[0].rows)
+                                .filter(f=>!f.classList.contains('filtradoTexto'));
+
+  const nFilas = filasSinFiltradoTexto.length;
+  const paginas = Math.ceil(nFilas/maxProductosPorPagina);
+
+  const primerProductoAMostrar = (pagina-1) * maxProductosPorPagina;
+  const ultimoProductoAMostrar = pagina * maxProductosPorPagina - 1;
+  
+  for (let i=0;i< filasSinFiltradoTexto.length; i++){
+      if (i>=primerProductoAMostrar && i<=ultimoProductoAMostrar) {
+          filasSinFiltradoTexto[i].classList.remove('ocultoPaginacion');
+      }
+      else {
+        filasSinFiltradoTexto[i].classList.add('ocultoPaginacion');
+      }
+  }
+  return obtenPaginacion(pagina,maxProductosPorPagina,paginas)
+}
+
 
 document.getElementById('filtra-producto')
     .addEventListener('input',(e)=>filtraProductosCon(e.target.value));
